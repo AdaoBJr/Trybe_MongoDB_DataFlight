ca67eac15d617de81642d2a58c9606bf4b49d9cb
denis-rossati <denis.rossati@hotmail.com>
2021-08-10 19:40:18 -0300

    desafio 23 - done

---
+++ challenges/desafio23.js
@@ -0,0 +1,11 @@
+db.voos.findOne(
+  {
+    "litrosCombustivel": { $not: { $gt: 1000 } },
+    "litrosCombustivel": { $exists: true },
+  },
+  {
+     "_id": false,
+     "vooId": true,
+     "litrosCombustivel": true,
+  }
+);
