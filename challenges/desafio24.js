b2c0595cdded0a580824db49a30262da5712a3fa
denis-rossati <denis.rossati@hotmail.com>
2021-08-10 19:54:12 -0300

    desafio 24 - done

---
+++ challenges/desafio24.js
@@ -0,0 +1,12 @@
+db.voos.findOne(
+  {
+    "litrosCombustivel": { $lt: 600 },
+    "empresa.nome": { $not: { $in: ["GOL", "AZUL"] } },
+  },
+  {
+     "_id": false,
+     "vooId": true,
+     "empresa.nome": true,
+     "litrosCombustivel": true,
+  }
+);
