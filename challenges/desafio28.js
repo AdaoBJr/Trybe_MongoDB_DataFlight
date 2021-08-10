bf91ca7bcadbfe5ee93f25da4cf97acc0187f0a2
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-08-10 20:38:38 -0300

    desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,16 @@
+const latam = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne({
+  empresa: latam,
+  totalVoosDomesticos: db.voos
+    .find({
+      "empresa.nome": latam,
+      natureza: "Doméstica",
+    })
+    .count(),
+});
+
+db.resumoVoos.findOne(
+  { empresa: latam },
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+);
