61b39c4a71f8ee8085531da3656d0de057a663f9
RafaMaths <rmathscoach@gmail.com>
2021-08-11 18:03:51 -0300

    challenge 27: done

---
+++ challenges/desafio27.js
@@ -0,0 +1,6 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.find({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" }).count(),
+});
+
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
