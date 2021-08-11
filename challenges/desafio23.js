51c410432af1d50203dfa68ca99416e6378c2fcd
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-08-11 08:59:14 -0300

    add exercicios 21, 22 e 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,9 @@
+db.voos.findOne(
+  { 
+    $and: [
+      { litrosCombustivel: { $lt: 1000 } }, 
+      { litrosCombustivel: { $exists: true } },
+    ], 
+  }, 
+  { _id: false, vooId: true, litrosCombustivel: true },
+);
