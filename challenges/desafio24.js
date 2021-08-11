d8f1174351f1dd3e017a97c3b03dbb7fc1a6e9f3
Cristian Bugs <cristian.bugs@gmail.com>
2021-08-11 17:25:03 -0300

    desafio24

---
+++ challenges/desafio24.js
@@ -0,0 +1,14 @@
+db.voos.findOne({
+    $and:
+    [
+        { litrosCombustivel: { $lte: 600 } }, 
+        {
+            $nor:
+            [
+                {"empresa.nome": "GOL"}, 
+                {"empresa.nome": "AZUL"}
+            ],
+        },
+        { litrosCombustivel: { $exists: true } },
+    ],
+}, { _id: false, vooId: true, "empresa.nome": true, litrosCombustivel: true });
