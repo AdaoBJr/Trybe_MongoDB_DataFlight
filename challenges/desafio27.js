7efc3f0877ed5141401f946507be3b399edef023
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-08-11 16:08:42 -0300

    desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,5 @@
+db.resumoVoos.insertOne({ empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.find({
+    "empresa.nome": "PASSAREDO", natureza: "Doméstica" }).count() });
+
+db.resumoVoos.find({}, { _id: 0 });
