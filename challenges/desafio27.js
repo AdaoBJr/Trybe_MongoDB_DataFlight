f5b8a3fc524493a4ac0c97925a8df0b0da8f505f
Abimaelr <abimaelr.albu@gmail.com>
2021-08-11 18:55:45 -0300

    feature 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,5 +1,11 @@
-db.voos.find({ $and: [
-    { "empresa.nome": { $eq: "PASSAREDO" } },
-    { "empresa.natureza": { $eq: "Doméstica" } },
-],
-}).count();
+db.resumoVoos.insertOne({
+    empresa: "PASSAREDO",
+    totalVoosDomesticos: db.voos.find({ $and: [
+        { "empresa.nome": { $eq: "PASSAREDO" } },
+        { "empresa.natureza": { $eq: "Doméstica" } },
+    ],
+    }).count(),
+});
+
+db.resumoVoos.findOne({ empresa: { $eq: "PASSAREDO" } },
+{ _id: 0, empresa: 1, totalVoosDomesticos: 1 });
