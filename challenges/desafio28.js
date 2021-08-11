17589968192cbbdeed9aa85c3632ef90584aff8f
root <diegoserafim1@gmail.com>
2021-08-11 19:14:50 -0300

    D28 - Retorna a quantidade total de voos de natureza Doméstica que a empresa LATAM AIRLINES BRASIL possui, via uso de uma nova coleção chamada resumoVoos.

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,10 @@
 // Retorna a quantidade total de voos de natureza Doméstica que a empresa LATAM AIRLINES BRASIL possui, via uso de uma nova coleção chamada resumoVoos.
+db.voos.find({ natureza: "Doméstica", "empresa.nome": "LATAM_AIRLINES_BRASIL" }).count();
 
+db.resumoVoos.insertOne({
+  empresa: "LATAM_AIRLINES_BRASIL",
+  totalVoosDomesticos: db.voos.find({
+     natureza: "Doméstica", "empresa.nome": "LATAM_AIRLINES_BRASIL" }).count(),
+});
+
+db.resumoVoos.findOne({ empresa: "LATAM_AIRLINES_BRASIL" }, { _id: 0 });
