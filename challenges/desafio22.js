2f3f21b8cf1aa1558df645cba211445778c0a316
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-08-11 19:04:51 -0300

    Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,3 @@
+db.voos.find({ $and: [{ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } }, 
+{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] }, 
+{ vooId: true, _id: false }).limit(1);
