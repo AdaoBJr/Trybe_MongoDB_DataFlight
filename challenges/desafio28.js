5884dba2d7809b24f079944b0b9743678cfd1eb0
renanpbraga <renanpbraga@gmail.com>
2021-08-11 19:16:39 -0300

    adiciona desafio28

---
+++ challenges/desafio28.js
@@ -0,0 +1,26 @@
+db.resumoVoos.insertOne(
+  {
+    empresa: "LATAM AIRLINES BRASIL",
+    totalVoosDomesticos: db.voos.count(
+      { $and:
+        [
+          {
+            natureza: "Doméstica",
+            "empresa.nome": "LATAM AIRLINES BRASIL",
+          },
+        ],
+      },
+    ),
+  },
+);
+
+db.resumoVoos.findOne(
+  {
+    empresa: "LATAM AIRLINES BRASIL",
+  },
+  {
+    _id: false,
+    empresa: true,
+    totalVoosDomesticos: true,
+  },
+);
