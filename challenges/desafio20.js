fc76210a2ce5b7bd219d9a5a9bce0b47becede53
Caio <thehaseol@gmail.com>
2021-08-11 14:57:08 -0300

    Exercicios 21 - 28

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,2 @@
 db.voos.find({ rtk: { $exists: false } }, { _id: false, vooId: true }).limit(1);
+
---
+++ challenges/desafio23.js
@@ -0,0 +1,12 @@
+db.voos
+  .find(
+    {
+      $and: [
+        { litrosCombustivel: { $exists: true } },
+        { litrosCombustivel: { $not: { $gt: 1000 } } },
+      ],
+    },
+    { _id: false, vooId: true, litrosCombustivel: true },
+  )
+  .limit(1);
+  
