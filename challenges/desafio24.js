92e24f6cd705f955bfb18cdfd308d177bbfad78e
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-11 16:26:30 -0300

    retorna id, nome e litros de combustivel do primeiro voo que nao seja gol ou azul e tenha menos de 600 litros

---
+++ challenges/desafio24.js
@@ -0,0 +1,9 @@
+db.voos.findOne({ $and: [
+  { litrosCombustivel: { $lte: 600 } },
+  { $nor: [
+    { "empresa.nome": "GOL" },
+    { "empresa.nome": "AZUL" },
+  ] },
+  { litrosCombustivel: { $exists: true } },
+  ],
+  }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
