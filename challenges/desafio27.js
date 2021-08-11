7a95717e841bf0c7035889c5d7183c4e5e679d6e
André Arnoni <andre_arnoni@yahoo.com.br>
2021-08-11 13:18:18 -0300

    executado requisito 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,4 @@
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: 4187 });
+
+db.resumoVoos
+.find({ empresa: "PASSAREDO" }, { _id: false, empresa: true, totalVoosDomesticos: true }).limit(1);
