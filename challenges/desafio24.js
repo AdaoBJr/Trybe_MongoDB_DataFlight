538fad628e56e93684d7a8e25701ac3d2b591ec7
root <diegoserafim1@gmail.com>
2021-08-11 18:15:50 -0300

    D25 - Remove todos os voos da empresa AZUL em que a quantidade de combustível seja menor do que 400.

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,4 +1,5 @@
 // Retorna o vooId, empresa.nome e litrosCombustivel do primeiro voo em que litrosCombustivel não seja maior do que 600 e a empresa não seja GOL ou AZUL, e o campo litrosCombustivel exista.
 db.voos.findOne({ litrosCombustivel: { $not: { $gte: 600 },
-$exists: true }, "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+$exists: true },
+"empresa.nome": { $nin: ["GOL", "AZUL"] } },
 { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
