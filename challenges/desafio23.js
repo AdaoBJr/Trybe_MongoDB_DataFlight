a4607b4d9d9bbbb82e35e88c9ba2286a321e1529
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-11 20:55:24 -0300

    Requisito 23 teste

---
+++ challenges/desafio23.js
@@ -0,0 +1,13 @@
+db.voos.findOne(
+  { litrosCombustivel: 
+    {
+      $not: { $gt: 1000 }, 
+      $exists: true,
+    },
+  },
+  {
+    _id: false,
+    vooId: true,
+    litrosCombustivel: true,
+  },
+);
