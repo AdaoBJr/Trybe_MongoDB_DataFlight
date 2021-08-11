05f9ba3c004f06954da641637f9e15fd5918fdb3
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-08-11 18:58:28 -0300

    fixes linter issues on req22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,9 @@
-db.voos.findOne({ $and: [{ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"]}}, {"aeroportoOrigem.sigla": "SBGR"}, {"aeroportoOrigem.sigla": "SBGR"}]}, {_id: 0, vooId: 1});
+db.voos.findOne({
+  $and:
+    [{
+      "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] }
+     },
+     { "aeroportoOrigem.sigla": "SBGR" },
+     { "aeroportoOrigem.sigla": "SBGR" }]
+    },
+    { _id: 0, vooId: 1 });
