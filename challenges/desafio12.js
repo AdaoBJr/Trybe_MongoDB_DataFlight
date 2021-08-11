c02b1c61287fb27560877aaff9930658499c6f1c
root <diegoserafim1@gmail.com>
2021-08-11 15:51:16 -0300

    D12 - quantidade de documentos em que o campo aeroportoDestino.pais seja igual a BRASIL, ARGENTINA ou CHILE.

---
+++ challenges/desafio12.js
@@ -0,0 +1,2 @@
+// quantidade de documentos em que o campo aeroportoDestino.pais seja igual a BRASIL, ARGENTINA ou CHILE.
+db.voos.find({ "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE"] } }).count(); // 133627
