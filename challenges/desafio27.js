bef6e510d49bbf617e400313dde93a639d33b0c6
renanpbraga <renanpbraga@gmail.com>
2021-08-11 18:37:38 -0300

    adiciona desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,25 @@
+use("dataFlights");
+db.resumoVoos.insertOne(
+  {
+    empresa: {
+      nome: "PASSAREDO",
+      totalVoosDomesticos: db.voos.find(
+        {
+          natureza: "Doméstica",
+          "empresa.nome": "PASSAREDO",
+        },
+      ).count(),
+    },
+  },
+);
+
+db.resumoVoos.find(
+  {
+    "empresa.nome": "PASSAREDO",
+  },
+  {
+    _id: false,
+    empresa: true,
+    totalVoosDomesticos: true,
+  }
+);
