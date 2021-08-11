d9a1a81a35dd969f69ca2562b6f09fbcbd8b2ffa
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-08-10 22:11:03 -0300

    desafio 22 e 23 completo

---
+++ challenges/desafio22.js
@@ -0,0 +1,1 @@
+db.voos.find({ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] }, $and: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] }, { _id: false, vooId: true }).limit(1);
