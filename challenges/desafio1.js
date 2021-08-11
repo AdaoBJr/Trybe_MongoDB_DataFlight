b7080004a6bf801891566696ff7716834582aef8
root <diegoserafim1@gmail.com>
2021-08-11 13:01:50 -0300

    D1 - Retorna a quantidade de documentos inseridos na coleção voos.

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,7 @@
-db.voos.find();
+// Para executar localmente os testes
+// ./scripts/evaluate.sh
+// Para executar somente 1 teste
+// ./scripts/evaluate.sh desafioN
+
+// Retorna a quantidade de documentos inseridos na coleção voos.
+db.voos.find().count(); // 157217
