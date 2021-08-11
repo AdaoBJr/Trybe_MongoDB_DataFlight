33ec42cbe6f7bdba7dec16b70f2cc700eb197b56
root <diegoserafim1@gmail.com>
2021-08-11 14:17:47 -0300

    D6 - escondendo o _id

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,2 @@
 // Retorna os campos empresa.sigla, empresa.nome e passageiros do voo com o campo vooId '756807'.
-db.voos.find({ vooId: 756807 }, { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1 }).pretty();
+db.voos.find({ vooId: 756807 }, { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 }).pretty();
