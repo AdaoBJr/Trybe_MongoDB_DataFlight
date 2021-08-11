effe69286b578bac2d22c36d323fc049ec09d9a9
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-08-10 21:22:54 -0300

    Finalizado 11,12,13

---
+++ challenges/desafio13.js
@@ -0,0 +1,9 @@
+db.voos.count(
+  {
+    $nor: [
+    { "aeroportoDestino.continente": "EUROPA" },
+    { "aeroportoDestino.continente": "ÁSIA" },
+    { "aeroportoDestino.continente": "OCEANIA" },
+  ],
+  },
+);
