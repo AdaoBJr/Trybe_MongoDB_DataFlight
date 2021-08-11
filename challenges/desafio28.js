efda833ed8cdb651228f20be3e8a22918603ca73
Antenor <antenorzapata@gmail.com>
2021-08-11 14:29:49 -0300

    req 28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -3,3 +3,4 @@ db.resumoVoos.insertOne({ empresa: NAME,
 totalVoosDomesticos:
 db.voos.count({ "empresa.nome": NAME, natureza: "Doméstica" }) });
 db.resumoVoos.find({ empresa: NAME }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
+g
