ad5c0c314a36326c238f2b7074d3d1c4718d73fb
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-08-11 19:19:57 -0300

    fixing req23 linter issues

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,12 @@
 db.voos.find({
-  $and: [{ litrosCombustivel: { $exists: true, $lte: 1000 }}]
-}, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
+  $and:
+    [{
+      litrosCombustivel:
+        {
+          $exists: true, $lte: 1000,
+        }
+      }]
+    },
+    {
+      _id: 0, vooId: 1, litrosCombustivel: 1
+    }).limit(1);
