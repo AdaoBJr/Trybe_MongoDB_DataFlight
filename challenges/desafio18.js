d3bfa2f2f0035d2505661a02baabbf7ae95aecd9
Cristian Bugs <cristian.bugs@gmail.com>
2021-08-11 15:51:04 -0300

    linter desafio 6 and 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,1 +1,2 @@
-db.voos.findOne({"passageiros.pagos": { $gt: 7000 } }, {"ano": true, "mes": true, vooId: true, _id: false})
+db.voos.findOne({ "passageiros.pagos": { $gt: 7000 } },
+{ "ano": true, "mes": true, vooId: true, _id: false })
