b870d5029b687ad1051bf1c73e019da637b8a718
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-11 18:29:11 -0300

    Retirado o use

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,6 +1,5 @@
-use("dataFlights");
 db.voos.find({ $and: 
-[{ litrosCombustivel: { $lte: 600 }}, { "empresa.nome": { $nin: ["GOL", "AZUL"] }}]
+[{ litrosCombustivel: { $lte: 600 } }, { "empresa.nome": { $nin: ["GOL", "AZUL"] } }],
 }, {
-  vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0
+  vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0,
 }).limit(1);
