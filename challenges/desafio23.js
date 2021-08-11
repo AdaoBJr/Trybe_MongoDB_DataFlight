ed93a92a048c0d9bbdc756d7848c3aac512f6e2b
Cristian Bugs <cristian.bugs@gmail.com>
2021-08-11 17:05:26 -0300

    desafio23 linter

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -4,4 +4,4 @@ db.voos.findOne({
       { litrosCombustivel: { $exists: true } },
     ],
   },
-    { _id: false, vooId: true, litrosCombustivel: true } );
+    { _id: false, vooId: true, litrosCombustivel: true });
