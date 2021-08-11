dace1838cebd7414d5c723adec7145e0d21b6832
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-08-11 19:55:45 -0300

    req23 refactoring

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,4 +1,4 @@
-db.voos.find({
+db.voos.findOne({
   $and:
     [{
       litrosCombustivel:
@@ -9,4 +9,4 @@ db.voos.find({
     },
     {
       _id: 0, vooId: 1, litrosCombustivel: 1,
-    }).limit(1);
+    });
