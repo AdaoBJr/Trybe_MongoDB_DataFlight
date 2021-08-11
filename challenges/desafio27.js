22cd690bb581edf66a2b41426ff789f5d6bb8f64
root <diegoserafim1@gmail.com>
2021-08-11 18:42:44 -0300

    D27 - Retorna a quantidade de voos de natureza Doméstica que a empresa PASSAREDO possui, via uso de uma nova coleção chamada resumoVoos.

---
+++ challenges/desafio27.js
@@ -0,0 +1,9 @@
+// Retorna a quantidade de voos de natureza Doméstica que a empresa PASSAREDO possui, via uso de uma nova coleção chamada resumoVoos.
+db.voos.find({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" }).count();
+
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", 
+totalVoosDomesticos: db.voos.find({ 
+  natureza: "Doméstica", "empresa.nome": "PASSAREDO" }).count(),
+});
+
+db.resumoVoos.findOne({ empesa: "PASSAREDO" }, { _id: 0 });
