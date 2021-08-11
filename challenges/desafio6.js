896e7cc51b464bae0b3beb1f143faf1a36e1144a
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-08-11 14:35:49 -0300

    desafio 27 completo

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,10 @@
-db.voos.find({ vooId: { $eq: 756807 } }, { _id: false, "empresa.sigla": true, "empresa.nome": true, passageiros: true });
+db.voos.find({
+  vooId: {
+    $eq: 756807,
+  },
+}, {
+  _id: false,
+  "empresa.sigla": true,
+  "empresa.nome": true,
+  passageiros: true,
+});
