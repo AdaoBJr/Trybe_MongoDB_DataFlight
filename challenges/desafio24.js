c73e621dbbcc3476002108a8906e36cc8185db96
Abimaelr <abimaelr.albu@gmail.com>
2021-08-11 18:26:55 -0300

    feature 23

---
+++ challenges/desafio24.js
@@ -0,0 +1,13 @@
+db.voos.find({ $and: [
+    { litrosCombustivel: { $exists: true } },
+    { litrosCombustivel: { $lte: 600 } },
+    { "empresa.nome": { $ne: "GOL" } },
+    { "empresa.nome": { $ne: "AZUL" } },
+],
+}, 
+{
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+}).limit(1);
