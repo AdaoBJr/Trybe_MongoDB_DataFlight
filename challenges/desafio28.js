53c7bf188dd5fbcfba7063a2bb88784603af4c3c
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-08-11 16:16:43 -0300

    lint

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,5 +1,5 @@
 db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL",
   totalVoosDomesticos: db.voos.find({
-    "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }).count() })
+    "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }).count() });
 
 db.resumoVoos.find({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0 });
