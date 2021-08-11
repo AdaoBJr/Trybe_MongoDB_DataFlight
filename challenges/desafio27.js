aa9c53f59b25aa4cd4ec37a5f951ef4cd64d61c2
root <diegoserafim1@gmail.com>
2021-08-11 18:58:22 -0300

    D27 - corrigindo erro em 'empresa

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,9 +1,9 @@
 // Retorna a quantidade de voos de natureza Doméstica que a empresa PASSAREDO possui, via uso de uma nova coleção chamada resumoVoos.
 db.voos.find({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" }).count();
 
-db.resumoVoos.insertOne({ empresa: "PASSAREDO", 
-totalVoosDomesticos: db.voos.find({ 
+db.resumoVoos.insertOne({ empresa: "PASSAREDO",
+totalVoosDomesticos: db.voos.find({
   natureza: "Doméstica", "empresa.nome": "PASSAREDO" }).count(),
 });
 
-db.resumoVoos.findOne({ empesa: "PASSAREDO" }, { _id: 0 });
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { _id: 0 });
