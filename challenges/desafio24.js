6d8b78f095d8d42509c81becf6ddc48576cfd534
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-08-11 19:43:14 -0300

    fixing req24 linter issues

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,1 +1,8 @@
-db.voos.findOne({$and: [{ litrosCombustivel: { $exists: true, $lte: 600}}, { "empresa.nome": { $nin: ["GOL", "AZUL"]}}]}, {_id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1});
+db.voos.findOne({
+  $and:
+    [{
+      litrosCombustivel:
+        { $exists: true, $lte: 600 }},
+        { "empresa.nome": { $nin: ["GOL", "AZUL"] } }]},
+        {_id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1});
+        
