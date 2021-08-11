ffb82d2beeab6f04200eb77ccfe4ab7f62a52551
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-11 17:03:38 -0300

    adiciona outro documento na collection resumo de voos

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,8 +1,9 @@
 const empresa = "PASSAREDO";
+const natureza = "Doméstica";
 const totalVoosDomesticos = db.voos.count({ $and: [
-  { "empresa.nome": empresa }, { natureza: "Doméstica" },
+  { "empresa.nome": empresa }, { natureza },
 ] });
 
 db.resumoVoos.insertMany([{ empresa, totalVoosDomesticos }]);
 
-db.resumoVoos.find({ empresa: "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
+db.resumoVoos.find({ empresa }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
---
+++ challenges/desafio28.js
@@ -0,0 +1,9 @@
+const empresa = "LATAM AIRLINES BRASIL";
+const natureza = "Doméstica";
+const totalVoosDomesticos = db.voos.count({ $and: [
+  { "empresa.nome": empresa }, { natureza },
+] });
+
+db.resumoVoos.insertMany([{ empresa, totalVoosDomesticos }]);
+
+db.resumoVoos.find({ empresa }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
