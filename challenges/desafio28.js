107e954ff782f1c79493dd2f893012e7ab0000cc
root <diegoserafim1@gmail.com>
2021-08-11 19:52:04 -0300

    D28 - declarado uma const

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,10 +1,12 @@
 // Retorna a quantidade total de voos de natureza Doméstica que a empresa LATAM AIRLINES BRASIL possui, via uso de uma nova coleção chamada resumoVoos.
-db.voos.find({ natureza: "Doméstica", "empresa.nome": "LATAM_AIRLINES_BRASIL" }).count();
+const LATAM_AIRLINES_BRASIL = "LATAM AIRLINES BRASIL";
+
+db.voos.find({ natureza: "Doméstica", "empresa.nome": LATAM_AIRLINES_BRASIL }).count();
 
 db.resumoVoos.insertOne({
-  empresa: "LATAM_AIRLINES_BRASIL",
+  empresa: LATAM_AIRLINES_BRASIL,
   totalVoosDomesticos: db.voos.find({
-     natureza: "Doméstica", "empresa.nome": "LATAM_AIRLINES_BRASIL" }).count(),
+     natureza: "Doméstica", "empresa.nome": LATAM_AIRLINES_BRASIL }).count(),
 });
 
-db.resumoVoos.findOne({ empresa: "LATAM_AIRLINES_BRASIL" }, { _id: 0 });
+db.resumoVoos.findOne({ empresa: LATAM_AIRLINES_BRASIL }, { _id: 0 });
