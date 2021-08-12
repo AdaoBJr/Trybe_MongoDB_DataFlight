cfe770c88cdd4ced3ff468f45700b365b9ef6b8f
arthur menezes <arthurmcorrea282@gmail.com>
2021-08-12 11:08:48 -0300

    desafios 22 e 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,7 @@
+db.voos.find(
+    {
+        litrosCombustivel: { $lt: 1000 },
+        litrosCombustivel: { $exists: true },
+    },
+    { vooId: 1, _id: 0, litrosCombustivel: 1 },
+  ).limit(1);
