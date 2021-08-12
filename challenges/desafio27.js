52ef131cf16e629d182e7cd2250970268d576edd
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-12 09:53:38 -0300

    Challenges 25 to 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,8 +1,6 @@
-db.voos.count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" });
-
 db.resumoVoos.insertOne({
     empresa: "PASSAREDO",
     totalVoosDomesticos: 4187,
 });
 
-db.resumoVoos.findOne({ empresa: "PASSAREDO" });
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { totalVoosDomesticos: 1 });
