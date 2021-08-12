624c778820838151fe49b7c27dee972b6d87835e
CIGR2021 <appencigr@gmail.com>
2021-08-12 17:27:46 -0300

    Desafio 24 - Fix Lint

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,9 +1,12 @@
 db.voos.find(
-    { litrosCombustivel: {
-        $exists: true,
-        $not: { $gt: 600 } },
+    {
+        litrosCombustivel: {
+            $exists: true,
+            $not: { $gt: 600 },
+        },
         "empresa.nome": {
-            $nin: ["GOL", "AZUL"]}
+            $nin: ["GOL", "AZUL"],
+        },
     },
     { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
 ).limit(1);
