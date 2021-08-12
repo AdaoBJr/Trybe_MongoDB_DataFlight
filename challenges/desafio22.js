197ab2c5cd4fe13bee50f684dc9b19d71a50dd6d
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-12 09:44:45 -0300

    Challenges 25 to 27

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,5 +1,5 @@
 db.voos
-    .findOne({
-        $or: [{ empresa: "DELTA AIRLINES" }, { empresa: "AMERICAN AIRLINES" }],
-        $and: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] },
+    .findOne({ $and:
+        { $or: [{ empresa: "DELTA AIRLINES" }, { empresa: "AMERICAN AIRLINES" }],
+        $and: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] } },
         { _id: 0, vooId: 1 });
