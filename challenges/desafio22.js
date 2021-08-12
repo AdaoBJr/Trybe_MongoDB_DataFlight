78d5937820960c1038dc41e3827a004c2e0570ce
CIGR2021 <appencigr@gmail.com>
2021-08-12 16:27:42 -0300

    Desafio 22 - Fix Lint

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -2,5 +2,7 @@ db.voos.find(
     {
         "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
         "aeroportoOrigem.sigla": "SBGR",
-        "aeroportoDestino.sigla": "KJFK"},
-    { _id: 0, vooId: 1 }).limit(1);
+        "aeroportoDestino.sigla": "KJFK"
+    },
+    { _id: 0, vooId: 1 },
+).limit(1);
