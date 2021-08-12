40473302d11dbab61eae850fddcd80e3db54d341
Jonathan Souza <souzajonathan570@gmail.com>
2021-08-11 23:18:20 -0300

    challenge 27 completed

---
+++ challenges/desafio27.js
@@ -0,0 +1,13 @@
+db.resumoVoos.insertMany([
+  {
+    empresa: "PASSAREDO",
+    totalVoosDomesticos: db.voos.count({
+      $and: [
+        { "empresa.nome": "PASSAREDO" },
+        { natureza: "Doméstica" },
+      ],
+    }),
+  },
+]);
+
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0 });
