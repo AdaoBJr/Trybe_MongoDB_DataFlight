1cea3aeb014c31ada4cbd828640e2c1d33e0be2e
CIGR2021 <appencigr@gmail.com>
2021-08-12 16:15:08 -0300

    Desafio 22 e 23 - Fix

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,7 +1,4 @@
 db.voos.find(
-    {
-        litrosCombustivel: { $exists: true },
-        litrosCombustivel: { $lt: 1000 },
-    },
+    { litrosCombustivel: { $exists: true, $lt: 1000 } },
     { _id: 0, vooId: 1, litrosCombustivel: 1 },
 ).limit(1);
