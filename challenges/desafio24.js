ec406f4638fc8d63af6034126488540f01ba5f19
Roger Franco <rogerfrancogalt@gmail.com>
2021-08-11 20:24:53 -0400

    Requisito 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,12 @@
+db.voos.findOne({
+  $and: [{
+    litrosCombustivel: { $lte: 600, $exists: true },
+
+  },
+{
+  "empresa.nome": { $nin: ["GOL", "AZUL"] },
+}],
+},
+  {
+    "empresa.nome": 1, litrosCombustivel: 1, vooId: 1, _id: 0,
+  });
