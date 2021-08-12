97cda4daaff13e291e208143e025a39495f8c49b
Flavio Andrade <foa12381@gmail.com>
2021-08-12 09:05:21 -0300

    resolve exercicio23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,6 +1,2 @@
-db.voos.findOne({ litrosCombustivel: { $lte: 1000, $exists: 1 } },
-  {
-    _id: 0,
-    vooId: 1,
-    litrosCombustivel: 1,
-  });
+db.voos.findOne({ litrosCombustivel: { $exists: 1, $lt: 1000 } },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 });
