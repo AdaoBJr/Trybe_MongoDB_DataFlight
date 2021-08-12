3839e94862797249db9ef367fede6c60bd12d145
Diegho <dieghonm@gmail.com>
2021-08-12 01:51:40 -0300

    Requisito 22 e 23

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,1 +1,1 @@
-db.voos.find({ "passageiros.pagos": 7000 }, { vooId: true, mes: true, ano: true, _id: false });
+db.voos.find({ "passageiros.pagos": { $gte: 7000 } }, { vooId: true, mes: true, ano: true, _id: false });
