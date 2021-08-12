a04e755c68d046326f872c9b2c5ca2fac015701a
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-11 21:30:02 -0300

    completed requirement 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,5 +1,4 @@
 db.voos.findOne({ 
-  litrosCombustivel: { $not: { $lte: 1000 } },
-  $exists: true, 
+  litrosCombustivel: { $not: { $gte: 1000 }, $exists: true },
 },
-{ _id: 0, vooId: 1, litrosComustivel: 1 });
+{ _id: 0, vooId: 1, litrosCombustivel: 1 });
