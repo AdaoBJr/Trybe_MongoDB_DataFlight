bb9248354cf6ed22be97e9599e4485c44008bc7b
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-08-15 12:59:49 -0300

    Arrumando querys 14,22 e 27 do projeot

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,3 +1,4 @@
-db.voos.find({ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }],
+db.voos.findOne({ $or: [{ "empresa.nome": "DELTA AIRLINES" }, 
+{ "empresa.nome": "AMERICAN AIRLINES" }],
 "aeroportoOrigem.sigla": "SBGR",
 "aeroportoDestino.sigla": "KJFK" }, { vooId: true, _id: false });
--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,18 +1,6 @@
-const empresa = "LATAM AIRLINES BRASIL";
-
-const total = db.voos.count({
-  natureza: "Doméstica",
-  "empresa.nome": empresa,
-});
-
+const total = db.voos.count({ "empresa.nome": "PASSAREDO",
+    natureza: "Doméstica" });
 db.resumoVoos.insertOne({
-  empresa,
-  totalVoosDomesticos: total,
-});
-
-db.resumoVoos.findOne({
-    empresa,
-  },
-  {
-    _id: false,
-  });
+    empresa: "PASSAREDO",
+    totalVoosDomesticos: total });
+db.resumoVoos.find({}, { _id: false });
