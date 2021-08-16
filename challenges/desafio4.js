6fc7b9cef6a38ee4601138dbc2f077d15f31c470
marcos <marcosd3wow@gmail.com>
2021-08-16 15:51:27 -0300

    corrigindo os req 3 e 4, verificar uso correto do count

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,2 @@
-db.voos.count({ "empresa.nome": "GOL" }).pretty();
+// db.voos.count({ "empresa.nome": "GOL" }).pretty(); -- mesma situaçao do req3, verificar o uso corredo do count
+db.voos.find({ "empresa.nome": "GOL" }).count();
