0f335451f70eb62169f6e603327a8d43df806886
Aderson Santos <assaderson@gmail.com>
2021-08-17 20:20:47 -0300

    Correção do desafio 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,6 +1,3 @@
-db.voos.find({ $or: [
-    { "aeroportoDestino.continente": { $ne: "EUROPA" } },
-    { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
-    { "aeroportoDestino.continente": { $ne: "OCEANIA" } },
-],
-}).count();
+db.voos.find({ $or: [{ "aeroportoDestino.continente": { $ne: "EUROPA" } },
+{ "aeroportoDestino.continente": { $ne: "ÁSIA" } },
+{"aeroportoDestino.continente": { $ne: "OCEANIA" } }, ]}).count();
