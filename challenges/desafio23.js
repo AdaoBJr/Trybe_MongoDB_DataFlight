6ad37d06924d0a11e201fc5cfaef68ca54af43e2
Pedro Toselli <pedrotrybe@gmail.com>
2021-08-17 17:22:03 -0300

    fix

---
+++ challenges/desafio23.js
@@ -0,0 +1,11 @@
+db.voos.findOne(
+  {
+    litrosCombustivel: { $not: { $gt: 1000 } },
+    litrosCombustivel: { $exists: true },
+  },
+  {
+     _id: 0,
+     vooId: 1,
+     litrosCombustivel: 1,
+  },
+);
