587c00eabe4aad8cc8a1bbe1613b4d902b4db2fa
Bruna Campos <92brunacampos@gmail.com>
2021-08-22 00:53:48 -0300

    desafio 10 - ignore _id

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,5 @@
-db.voos.find({ "empresa.nome": "GOL" }, { vooId: 1,
+db.voos.find({ "empresa.nome": "GOL" }, { _id: 0,
+vooId: 1,
 "empresa.nome": 1,
 "aeroportoOrigem.nome": 1,
 "aeroportoDestino.nome": 1,
