1bdc75d6cdd2fab5bb3f83995dc24698c8aefb61
Bruna Campos <92brunacampos@gmail.com>
2021-08-22 12:17:33 -0300

    desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,4 @@
+db.voos.find({ "empresa.nome": { $in: ["AMERICAN AIRLINES", "DELTA AIRLINES"] }, 
+"aeroportoOrigem.sigla": "SBGR", 
+"aeroportoDestino.sigla": "KJFK",
+}, { _id: 0, vooId: 1 }).limit(1).pretty();
