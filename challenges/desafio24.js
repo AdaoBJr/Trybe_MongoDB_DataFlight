b0794a57902e07552aeb4eab6d6bc1fef87c10df
Bruna Campos <92brunacampos@gmail.com>
2021-08-22 12:38:59 -0300

    desafio 24, add  (apesar da saida ser a mesma)

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,2 +1,3 @@
-db.voos.find({ litrosCombustivel: { $lte: 600 }, "empresa.nome": { $nin: ["GOL", "AZUL"] },
+db.voos.find({ litrosCombustivel: { $exists: true, $lte: 600 }, 
+"empresa.nome": { $nin: ["GOL", "AZUL"] },
 }, { _id: 0, vooId: 1, litrosCombustivel: 1, "empresa.nome": 1 }).limit(1).pretty();
