c4590fc5cda1727ec6f68cdbd1423d44b1e9f887
Ricardo <ricardo.rals.dev@gmail.com>
2021-08-31 16:32:57 -0300

    Fix Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
-db.voos.find({ "empresa.nome": "GOL", ano: { $eq: 2017 } },
+db.voos.find({ "empresa.nome": "GOL", ano: 2017 },
   {
     vooId: 1,
     "empresa.nome": 1,
@@ -6,5 +6,5 @@ db.voos.find({ "empresa.nome": "GOL", ano: { $eq: 2017 } },
     "aeroportoDestino.nome": 1,
     mes: 1,
     ano: 1,
-    _id: 0
+    _id: 0,
   }).limit(10);
