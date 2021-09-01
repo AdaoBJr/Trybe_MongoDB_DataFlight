8567639c74279d63bf75be6fa9265c2c23a00b91
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-08-31 23:32:58 -0300

    Desafio 26, 27, 28

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,6 +1,1 @@
-db.voos.findOne({
-  litrosCombustivel: { $lte: 600 },
-  "empresa.nome": { $nin: ["GOL", "AZUL"] }
-},
-{ _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
-);
+db.voos.deleteMany({ "empresa.nome": "AZUL", litrosCombustivel: { $lt: 400 } });
