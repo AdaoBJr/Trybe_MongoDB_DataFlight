cf820d1da05ec20f5d576647e4a95a307eb3c625
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-09-03 07:08:02 -0400

    Ajustado erro.

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -3,10 +3,5 @@ db.voos.findOne(
       litrosCombustivel: { $exists: true, $lt: 600 },
       "empresa.nome": { $nin: ["GOL", "AZUL"] },
     },
-    {
-        "empresa.nome": 1, 
-        litrosCombustivel: 1, 
-        vooId: 1,
-        id: 0,
-    },
+    { "empresa.nome": 1, litrosCombustivel: 1, vooId: 1, _id: 0 },
   );
